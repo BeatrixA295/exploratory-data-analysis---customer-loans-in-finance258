4ed742e6fe728eec28e1de8fe83aabee8302592c
Jkswm <beatrixawuah@outlook.com>
2023-12-07 14:37:13 +0000

    "Updates up until reduce skedness"

--- db_utils.py
+++ db_utils.py
@@ -51,4 +51,83 @@ print(loaded_data)
 loans_df = pd.read_csv('loan_payments_data.csv')
 loans_df.head(10)
 loans_df
+
+print(loans_df.dtypes)
 # %%
+
+
+class DataTransformer:
+    def __init__(self, df):
+        self.df = df.copy()  # Create a copy of the DataFrame
+        
+    def transform_data(self):
+        self._convert_to_datetime(['issue_date', 'earliest_credit_line'])
+        self._extract_numeric_and_convert('employment_length', r'(\d+)')
+        self._extract_numeric_and_convert('term', r'(\d+)')
+        self._convert_to_categorical(['verification_status', 'grade', 'sub_grade', 'home_ownership', 'loan_status', 'payment_plan', 'purpose', 'application_type'])
+        self._handle_missing_and_convert_to_int(['mths_since_last_delinq', 'mths_since_last_record', 'mths_since_last_major_derog'])
+        
+    def _convert_to_datetime(self, columns):
+        for col in columns:
+            self.df[col] = pd.to_datetime(self.df[col], format='%b-%Y', errors='coerce')
+            
+    def _extract_numeric_and_convert(self, column, pattern):
+        self.df[column] = self.df[column].str.extract(pattern).astype(float)
+        
+    def _convert_to_categorical(self, columns):
+        for col in columns:
+            self.df[col] = self.df[col].astype('category')
+            
+    def _handle_missing_and_convert_to_int(self, columns):
+        for col in columns:
+            self.df[col] = self.df[col].fillna(0).astype(int) 
+            
+
+
+loans_df_transformed = DataTransformer(loans_df)
+loans_df_transformed.transform_data()
+loans_df_transformed._convert_to_datetime()
+loans_df_transformed._extract_numeric_and_convert()
+loans_df_transformed._handle_missing_and_convert_to_int
+            
+            
+class DataFrameInfo:
+    def __init__(self, df):
+        self.df = df.copy()  # Create a copy of the DataFrame
+        
+    def describe_columns(self):
+        return self.df.dtypes
+    
+    def extract_stats(self):
+        return self.df.describe()
+    
+    def count_distinct_values(self):
+        return self.df.select_dtypes(include='category').nunique()
+    
+    def print_shape(self):
+        print("DataFrame shape:", self.df.shape)
+        
+    def count_null_values(self):
+        return self.df.isnull().sum()
+    
+    # Any other custom methods or EDA tasks can be added here
+    
+   #%%
+class DataTransformer2:
+    def __init__(self, df):
+     self.df = df.copy()  # Create a copy of the DataFrame
+    def reduce_skewness(self, threshold=1):
+        numerical_cols = self.df.select_dtypes(include=['float64', 'int64']).columns
+        skewed_cols = self.df[numerical_cols].apply(lambda x: x.skew()).sort_values(ascending=False)
+        skewed_cols = skewed_cols[abs(skewed_cols) > threshold]
+
+        for col in skewed_cols.index:
+            if self.df[col].min() > 0:  # Transformation for positive values
+                self.df[col] = np.log1p(self.df[col])
+            else:
+                self.df[col] = np.sign(self.df[col]) * np.log1p(abs(self.df[col]))
+                
+loans_df_transformed2 = DataTransformer2(loans_df)
+loans_df_transformed2.transform_data()
+    
+
