41de1440857b2e17d0b2baaa9fec7b89e1c75e77
Jkswm <beatrixawuah@outlook.com>
2023-12-20 20:51:12 +0000

    "Revamps Notebook Layout"

--- data_cleaning_utils.py
+++ data_cleaning_utils.py
@@ -1,10 +1,12 @@
 from db_utils import NullPercentageCalculator
+import matplotlib.pylab as plt
+import pandas as pd
+
 
 columns = ['id', 'member_id', 'loan_amount', 'funded_amount', 'funded_amount_inv', 'int_rate', 'instalment', 'annual_inc', 'dti', 'delinq_2yrs', 'inq_last_6mths', 'open_accounts', 'total_accounts', 'out_prncp', 'out_prncp_inv', 'total_payment', 'total_payment_inv', 'total_rec_prncp', 'total_rec_int', 'total_rec_late_fee', 'recoveries', 'collection_recovery_fee', 'last_payment_amount', 'collections_12_mths_ex_med', 'policy_code']
 def handle_missing_values(loans_df, drop_threshold=50, impute_threshold_low=0, impute_threshold_high=10, drop_rows_threshold=0.01):
 
     null_calculator = NullPercentageCalculator(loans_df)
-     
     #Count of Null Values 
     null_values = null_calculator.count_null_values()
     print("Initial Null Count:\n", null_values)
@@ -27,3 +29,24 @@ def handle_missing_values(loans_df, drop_threshold=50, impute_threshold_low=0, i
     # Final null counts
     null_count_updated = null_calculator.df.isnull().sum()
     print("Final Null Counts:\n", null_count_updated)
+    return clean_loans_data
+
+def visualise_null_removal(loans_df, cleaned_loans_data):
+        visualise_null_removal(loans_df, cleaned_loans_data)
+        null_counts_original = loans_df.isnull().sum()
+        null_counts_cleaned = cleaned_loans_data.isnull().sum()
+
+        plt.figure(figsize=(14, 8))  
+        plt.bar(null_counts_original.index, null_counts_original, alpha=0.5, label='Before Cleaning')
+        plt.bar(null_counts_cleaned.index, null_counts_cleaned, alpha=0.5, label='After Cleaning')
+        plt.xticks(rotation=90)
+        plt.xlabel('Columns')
+        plt.ylabel('Number of NULL values')
+        plt.title('Comparison of NULL values before and after cleaning')
+        plt.legend()
+        plt.tight_layout()
+        plt.show()
+        
+
+    
+ 
--- db_utils.py
+++ db_utils.py
@@ -1,8 +1,7 @@
-import yaml
+import yaml 
 import pandas as pd
 import psycopg2
 from sqlalchemy import create_engine
-import sqlalchemy
 import seaborn as sns
 import missingno as msno 
 import matplotlib.pyplot as plt
@@ -10,26 +9,26 @@ with open('credentials.yaml', 'r') as f:
     credentials = yaml.safe_load(f)
 #%%
 class RDSDatabaseConnector:
-    def __init__(self, credentials):
-        self.credentials = credentials
+    def __init__(df, credentials):
+        df.credentials = credentials
 
     #PROBLEM WITH STRING LITERAL EU-WEST1 AND INDENTATION IN NEXT CODE
-    def _create_engine(self):
-        engine = create_engine(f"postgresql+psycopg2://{self.credentials['RDS_USER']}:{self.credentials['RDS_PASSWORD']}@{self.credentials['RDS_HOST']}:{self.credentials['RDS_PORT']}/{self.credentials['RDS_DATABASE']}")
+    def _create_engine(df):
+        engine = create_engine(f"postgresql+psycopg2://{df.credentials['RDS_USER']}:{df.credentials['RDS_PASSWORD']}@{df.credentials['RDS_HOST']}:{df.credentials['RDS_PORT']}/{df.credentials['RDS_DATABASE']}")
         return engine
     
-    def initialise_engine(self):
-        self.engine = self._create_engine()
+    def initialise_engine(df):
+        df.engine = df._create_engine()
 
-    def data_extraction(self, table_name='loan_payments'):
+    def data_extraction(df, table_name='loan_payments'):
         query = f"SELECT * FROM loan_payments;"
-        data = pd.read_sql(query, self.engine)
+        data = pd.read_sql(query, df.engine)
         return data
     
-    def save_to_file(self, data, file_path='loan_payments_data.csv'):
+    def save_to_file(df, data, file_path='loan_payments_data.csv'):
         data.to_csv(file_path, index=False)
 
-    def load_loan_data(self, file_path='loan_payments_data.csv'):
+    def load_loan_data(df, file_path='loan_payments_data.csv'):
         try:
             return pd.read_csv(file_path)
         except FileNotFoundError:
@@ -45,137 +44,134 @@ loan_data.save_to_file(data=loan_data.data_extraction('loan_payments'), file_pat
 loaded_data = loan_data.load_loan_data('loan_payments_data.csv')
 print(loaded_data) 
 
-loans_df = pd.read_csv('loan_payments_data.csv')
+df = pd.read_csv('loan_payments_data.csv')
 #%%
 
 class DataFrameTransform:
-    def __init__(self, df):
-        self.df = df.copy()  # Create a copy of the DataFrame
+       
         
-    def transform_data(self):
-        skewed_columns = ['id', 'member_id', 'loan_amount', 'funded_amount_inv', 'annual_inc', 'delinq_2yrs', 'inq_last_6mths','mths_since_last_delinq', 'mths_since_last_record','open_accounts', 'total_accounts','collections_12_ex_med','mths_since','last_major_derog','policy_code', 'int_rate', 'instalment','dti', 'out_prncp', 'total_payment_inv,total_rec_prncp','total_rec_int','total_rec_late_fees','recoveries','collection_recovery_fee' ,'last_payment_amount']
-        date_columns =  ['issue_date', 'earliest_credit_line','last_payment_date',"last_credit_pull_date"]
-        self._convert_to_datetime(date_columns)
-        self._extract_numeric_and_convert('employment_length', r'(\d+)')
-        self._extract_numeric_and_convert('term', r'(\d+)')
-        self._convert_to_categorical(['grade', 'sub_grade', 'loan_status', 'payment_plan', 'purpose', 'application_type'])
-        self._handle_missing_and_convert_to_int(['mths_since_last_delinq', 'mths_since_last_record', 'mths_since_last_major_derog'])
+    def _convert_to_datetime(df, date_columns):
+        for col in date_columns:
+            df[col] = pd.to_datetime(df[col], format='%b-%Y', errors='coerce')
+            
+    def drop_rows_date_columns(df):
+        date_columns = ['issue_date', 'earliest_credit_line', 'last_payment_date', "last_credit_pull_date"]
+        for col in date_columns:
+            df_copy = df.drop('column_to_drop', axis=1).copy()       
+            return df_copy
+            
+    def _extract_numeric_and_convert(df, column, pattern):
+        df[column] = df[column].str.extract(pattern).astype(float)
         
-    def _convert_to_datetime(self, columns):
+    def _convert_to_categorical(df, columns):
         for col in columns:
-            self.df[col] = pd.to_datetime(self.df[col], format='%b-%Y', errors='coerce')
+            df[col] = df[col].astype('category')
             
-    def _extract_numeric_and_convert(self, column, pattern):
-        self.df[column] = self.df[column].str.extract(pattern).astype(float)
-        
-    def _convert_to_categorical(self, columns):
+    def _handle_missing_and_convert_to_int(df, columns):
         for col in columns:
-            self.df[col] = self.df[col].astype('category')
+            df[col] = df[col].fillna(0).astype(int) 
             
-    def _handle_missing_and_convert_to_int(self, columns):
-        for col in columns:
-            self.df[col] = self.df[col].fillna(0).astype(int) 
-            
-    def reduce_skewness(self, threshold=1):
-        numerical_cols = self.df.select_dtypes(include=['float64', 'int64']).columns
-        skewed_cols = self.df[numerical_cols].apply(lambda x: x.skew()).sort_values(ascending=False)
+    def reduce_skewness(df, threshold=1):
+        numerical_cols = df.select_dtypes(include=['float64', 'int64']).columns
+        skewed_cols = df[numerical_cols].apply(lambda x: x.skew()).sort_values(ascending=False)
         skewed_cols = skewed_cols[abs(skewed_cols) > threshold]
 
         for col in skewed_cols.index:
-            if self.df[col].min() > 0:  # Transformation for positive values
-                self.df[col] = np.log1p(self.df[col])
+            if df[col].min() > 0:  # Transformation for positive values
+                df[col] = np.log1p(df[col])
             else:
-                self.df[col] = np.sign(self.df[col]) * np.log1p(abs(self.df[col]))
+                df[col] = np.sign(df[col]) * np.log1p(abs(df[col]))
                 
 #%% 
 class NullPercentageCalculator:
-        def __init__(self, df):
-            self.df = df
-                            
-        def count_null_values(self):
-            return self.df.isnull().sum()
+        def list_columns(df):
+            columns_list = list(df.columns)
+            return columns_list        
 
-        def calculate_null_percentage(self,column):
-            percent_missing = self.df[column].isnull().sum() * 100 / len(self.df)
+        def count_null_values(df):
+            return df.isnull().sum()
+
+        def calculate_null_percentage(df, column):
+            columns = df.columns 
+            ['id', 'member_id', 'loan_amount', 'funded_amount', 'funded_amount_inv', 'int_rate', 'instalment', 'annual_inc', 'dti', 'delinq_2yrs', 'inq_last_6mths', 'open_accounts', 'total_accounts', 'out_prncp', 'out_prncp_inv', 'total_payment', 'total_payment_inv', 'total_rec_prncp', 'total_rec_int', 'total_rec_late_fee', 'recoveries', 'collection_recovery_fee', 'last_payment_amount', 'collections_12_mths_ex_med', 'policy_code']
+            
+            percent_missing = df[column].isnull().sum() * 100 / len(df)
             return percent_missing
 
-        def drop_high_null_columns(self, threshold=50):
-            percent_missing = self.df.isnull().sum() * 100 / len(self.df)
+        def drop_high_null_columns(df, threshold=50):
+            percent_missing = df.isnull().sum() * 100 / len(df)
             columns_to_drop = percent_missing[percent_missing > threshold].index.tolist()
-            self.df.drop(columns=columns_to_drop, inplace=True)
+            df.drop(columns=columns_to_drop, inplace=True)
 
-        def impute_nulls(self, threshold_low=0, threshold_high=10):
-            percent_missing = self.df.isnull().sum() * 100 / len(self.df)
+        def impute_nulls(df, threshold_low=0, threshold_high=10):
+            percent_missing = df.isnull().sum() * 100 / len(df)
             columns_to_impute = percent_missing[(percent_missing > threshold_low) & (percent_missing < threshold_high)].index.tolist()
 
             for col in columns_to_impute:
-                if self.df[col].dtype == 'object':
-                    self.df[col].fillna(self.df[col].mode()[0], inplace=True)
+                if df[col].dtype == 'object':
+                    df[col].fillna(df[col].mode()[0], inplace=True)
                 else:
-                    if abs(self.df[col].skew()) > 1:
-                        self.df[col].fillna(self.df[col].median(), inplace=True)
+                    if abs(df[col].skew()) > 1:
+                        df[col].fillna(df[col].median(), inplace=True)
                     else:
-                        self.df[col].fillna(self.df[col].mean(), inplace=True)
-
-        def drop_rows_date_columns(self, threshold=0.01):
+                        df[col].fillna(df[col].mean(), inplace=True)
+                        
+        def drop_rows_date_columns(df, threshold=0.01):
             date_columns = ['issue_date', 'earliest_credit_line', 'last_payment_date', "last_credit_pull_date"]
             for col in date_columns:
-                if col in self.df.columns:
-                    null_percentage = self.df[col].isnull().mean()
+                if col in df.columns:
+                    null_percentage = df[col].isnull().mean()
                     if null_percentage > threshold:
-                        self.df.dropna(subset=[col], inplace=True)
+                        df.dropna(subset=[col], inplace=True)
+
+
 
 #%%
             
 class DataFrameInfo:
-        def __init__(self, df):
-            self.df = df.copy()  # Create a copy of the DataFrame
-            
-        def describe_columns(self):
-            return self.df.dtypes
         
-        def extract_stats(self):
-            return self.df.describe()
-        
-        def count_distinct_values(self):
-            distinct_values = {}
-            for col in self.df.columns:
-                distinct_values[col] = self.df[col].nunique()
-            return distinct_values   
-        
-        def print_shape(self):
-            print("DataFrame shape:")
-            return self.df.shape
+        print("\nStatistical Values:")
+        print(df.describe)
+        print ('Column_Types: ')
+        column_types = df.dtypes
+        print(column_types)
+        print('Number of Distinct Values: ')
+        distinct_values = {}
+        for col in df.columns:
+            distinct_values[col] = df[col].nunique()
+        print((distinct_values))
+        print('DataFrame Shape')
+        print(df.shape)
 
     
     # Any other custom methods or EDA tasks can be added here
 #%%
-class Plotter:
-                                                        
-                def show_matrix_before(df):
-                    msno.matrix(df)
-                    plt.title('Missing Values Matrix Before Handling')
-                    plt.show()
+class Plotter:                        
+    
+    def show_matrix_before(df):
+        msno.matrix(df)
+        plt.title('Missing Values Matrix Before Handling')
+        plt.show()
 
-                def show_heatmap_before(df):
-                    msno.heatmap(df)
-                    plt.title('Missing Values Heatmap Before Handling')
-                    plt.show()
+    def show_heatmap_before(df):
+        msno.heatmap()
+        plt.title('Missing Values Heatmap Before Handling')
+        plt.show()
 
-                def show_matrix_after(df):
-                        msno.matrix(df)
-                        plt.title('Missing Values Matrix After Handling')
-                        plt.show()
+    def show_matrix_after(df):
+            msno.matrix(df)
+            plt.title('Missing Values Matrix After Handling')
+            plt.show()
 
-                def show_heatmap_after(df):
-                    msno.heatmap(df)
-                    plt.title('Missing Values Heatmap After Handling')
-                    plt.show()
+    def show_heatmap_after(df):
+        msno.heatmap(df)
+        plt.title('Missing Values Heatmap After Handling')
+        plt.show()
 
-                
-                def histogram(df, column, bins):
-                    plt.hist(df[column], bins=bins)
-                    plt.tight_layout()
-                    plt.show()
+    
+    def histogram(df, column, bins):
+        plt.hist(df[column], bins=bins)
+        plt.tight_layout()
+        plt.show()
 
 # %%
